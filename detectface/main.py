af06187d6a7e0e8c83544315fb2a20fa2243e5a0
xlwu <342541206@qq.com>
2020-07-09 22:36:48 +0800

    Initial commit

---
+++ detectface/main.py
@@ -0,0 +1,10 @@
+import os
+from PIL import Image
+from face_dect_recong.align.detector import detect_faces
+from face_dect_recong.align.visualization_utils import show_results
+
+__cwd__ = os.path.dirname(os.path.abspath(__file__))
+
+img = Image.open('../data/other_my_face/my/my/myf112.jpg')
+bounding_boxes, landmarks = detect_faces(img)  # detect bboxes and landmarks for all faces in the image
+show_results(img, bounding_boxes, landmarks)  # visualize the results
